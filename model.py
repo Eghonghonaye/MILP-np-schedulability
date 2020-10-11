5441d2660ee015a10f6d796359c780de21417238
Björn Brandenburg <bbb@mpi-sws.org>
2020-10-11 15:12:25 +0200

    generate demand-bound constraints

--- model.py
+++ model.py
@@ -1,6 +1,51 @@
 from gurobipy import *
 from itertools import combinations
 
+
+def demand_of_job(release, cost, deadline, a, b):
+    "demand of a job in the interval [a, b)"
+    pre  = max(0, a - release)  # time before a after release
+    post = max(0, deadline - b) # time after  b after deadline
+    return max(0, cost - pre - post)
+
+def demand_intervals_before(b, releases, deadlines, costs):
+    work = 0
+    # for efficiency reasons, this is doing an intertwined double iteration
+    jobs = iter(sorted(range(len(releases)), key=lambda i: releases[i], reverse=True))
+    points = iter(sorted(set((r for r in releases if r < b)), reverse = True))
+    cur_job = next(jobs)
+    a = next(points)
+    while True:
+        # count work of relevant jobs
+        if releases[cur_job] >= a and deadlines[cur_job] <= b:
+            work += costs[cur_job]
+        # we move from later to earlier releases, so if we hit this condition,
+        # we're done with a
+        if releases[cur_job] < a:
+            yield (work / (b - a), a)
+            # advance point
+            a = next(points)
+        else:
+            # advance job
+            try:
+                cur_job = next(jobs)
+            except StopIteration:
+                # ran out of jobs
+                # yield current point and then stop
+                yield (work / (b - a), a)
+                break
+
+def max_demand_interval_before(b, releases, deadlines, costs):
+    demand_ratio, a = max(demand_intervals_before(b, releases, deadlines, costs),
+                          default=0)
+    return demand_ratio, (a, b)
+
+def intervals_of_interest(releases, deadlines, costs):
+    return ((a, b) for ratio, (a, b) in
+                (max_demand_interval_before(d, releases, deadlines, costs)
+                 for d in set(deadlines))
+            if ratio > 1)
+
 def make_gurobi_milp(releaseTimes, deadlines, executionTimes, ncores, M, name='RAP'):
     # declare and init model
     m = Model(name)
@@ -33,7 +78,7 @@ def make_gurobi_milp(releaseTimes, deadlines, executionTimes, ncores, M, name='R
                not (releaseTimes[i] >= deadlines[j] or
                     releaseTimes[j] >= deadlines[i])
 
-    for seqno, (i, j) in enumerate(combinations(range(njobs), 2)):
+    for i, j in combinations(range(njobs), 2):
         if relevant(i, j):
             min_start = m.addVar(name = 'minStart[%d,%d]' % (i, j))
             max_fin   = m.addVar(name = 'maxStart[%d,%d]' % (i, j))
@@ -48,6 +93,19 @@ def make_gurobi_milp(releaseTimes, deadlines, executionTimes, ncores, M, name='R
                           <= max_fin + M * (1 - x[i,k]) + M * (1 - x[j,k])
                           for k in range(ncores)))
 
+    # Generate demand constraints --- these are strictly speaking redundant,
+    # but serve to guide the solver.
+    def demand_constraint(a, b, k):
+        coeffs = [demand_of_job(releaseTimes[i], executionTimes[i], deadlines[i], a, b)
+                  for i in range(njobs) if releaseTimes[i] < b and deadlines[i] > a]
+        vars   = [x[i,k] for i in range(njobs) if releaseTimes[i] < b and deadlines[i] > a]
+        return LinExpr(coeffs, vars) <= (b - a)
+
+    for (a, b) in intervals_of_interest(releaseTimes, deadlines, executionTimes):
+        m.addConstrs((demand_constraint(a, b, k) for k in range(ncores)),
+                      'demand-%d-%d' % (a, b))
+
+    # we just want a feasible solution
     m.setObjective(0)
 
     return m
